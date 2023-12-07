2856aed772a3152b237ed1f25a98f9f07d168380
MariaSolGonzalez <solgonzalez24@hotmail.com>
2023-12-07 15:49:32 -0300

    se le agrego name class en html y funcionalidad con js al boton de cancelar en la vista de nueva operacion

--- main.js
+++ main.js
@@ -3,20 +3,29 @@ const all = (selector) => document.querySelectorAll(selector);
 const randomId = () => self.crypto.randomUUID();
 
 
-const hideShowView = (showView) => { //hago una funcion que muestre y oculte las vistas
+const showView = (view) => { //hago una funcion que muestre y oculte las vistas
     all(".view").forEach((view) => { //primero digo que por cda clase que tenga en su nombre view (todas)
       view.classList.add("hidden"); //entonces me agregue la clase hidden
     });
-    just(`.${showView}`).classList.remove("hidden"); //y luego le decimos che, pero a esta en especifico, sacale ese hidden y mostramela
+    just(`.${view}`).classList.remove("hidden"); //y luego le decimos che, pero a esta en especifico, sacale ese hidden y mostramela
   };
 
 
-// FUNCIONALIDAD DEL NAVBAR
-just("#btn-balance-navb").addEventListener("click", () => hideShowView("main-page"))//escucha el click sobre boton de balance y esconde todas las vistas excepto la de balance
+// FUNCIONALIDAD DEL NAVBAR *****************************************************************************************
+just("#btn-balance-navb").addEventListener("click", () => showView("main-page"))//escucha el click sobre btn de balance y esconde todas las vistas excepto la de balance
 
-just("#btn-newOp").addEventListener("click", () => hideShowView("section-newOperation"))//escucha el click sobre boton de nueva operacion y esconde todas las vistas excepto la de nueva operacion
+just("#btn-newOp").addEventListener("click", () => showView("section-newOperation"))//escucha el click sobre btn de nueva operacion y esconde todas las vistas excepto la de nueva operacion
 
-just("#btn-category-navb").addEventListener("click", () => hideShowView("section-category"))//escucha el click sobre boton de categorias y esconde todas las vistas excepto la de categorias
+just("#btn-category-navb").addEventListener("click", () => showView("section-category"))//escucha el click sobre btn de categorias y esconde todas las vistas excepto la de categorias
 
-just("#btn-reports-navb").addEventListener("click", () => hideShowView("section-reports"))//escucha el click sobre boton de reportes y esconde todas las vistas excepto la de reportes
+just("#btn-reports-navb").addEventListener("click", () => showView("section-reports"))//escucha el click sobre btn de reportes y esconde todas las vistas excepto la de reportes
 
+// FUNCIONALIDAD DE BALANCE *****************************************************************************************
+
+// FUNCIONALIDAD DE FILTROS *****************************************************************************************
+
+// FUNCIONALIDAD DE OPERACIONES *****************************************************************************************
+
+// FUNCIONALIDAD DE NUEVA OPERACION *****************************************************************************************
+// -----------BOTONES CANCELAR Y AGREGAR 
+just("#btn-cancel-newOp").addEventListener("click", () => showView("main-page")) //escucha el click sobre btn cancelar en nueva op y devuelve solo la vista principal
