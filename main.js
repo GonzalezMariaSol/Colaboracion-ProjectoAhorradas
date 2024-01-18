32026744c91b35d7c080475158990e8cefe42f33
MariaSolGonzalez <mariasolgonzalez240@hotmail.com>
2024-01-18 17:00:34 -0300

    arreglos de conflictos con reportes

--- main.js
+++ main.js
@@ -198,6 +198,7 @@ const validateForm = (e) => {
 // FUNCIONALIDAD DE BALANCE *****************************************************************************************
 const getEarningsBalance = () => {
   let totalEarnings = 0;
+  just(".full-earnings").innerHTML = `${totalEarnings}`;
   if (getInfo("Operations")) {
     for (const operacion of getInfo("Operations")) {
       if (operacion.tipo === "ganancia") {
